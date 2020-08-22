b129091dd40e2bf2c1c3e7051b805bc489efc9e4
kenzotakamori <marceloktakamori@gmail.com>
2020-08-22 18:43:04 -0300

    Selected Vehicle + Modals

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
     const url = 'http://127.0.0.1:3000/';
     var availableVehicles = [];
     var vehiclesToBeShown = [];
+    var selectedVehicle = { id: null };
 
     function init() {
+        getVehicleTemplate();
         getAvailableVehicles();
         addFunctionToButtons();
         addModalReferences();
+    }
+
+    var vehicleTemplate;
+    function getVehicleTemplate() {
+        $.get("templates/vehicle-tile.html", function(data){
+            vehicleTemplate = data;
+        });
     }
 
     function getAvailableVehicles() {
@@ -22,7 +31,8 @@ $(document).ready(function(){
         vehiclesToBeShown.forEach(function(item) {
             let tile = buildVehicleTile(item);
             $('.list-options').append(tile);
-        })
+        });
+        selectVehicleOnClick();
     }
 
     function clearListOptions() {
@@ -30,77 +40,69 @@ $(document).ready(function(){
     }
 
     function buildVehicleTile(item) {
-        //copy from template
-        let tile;
-        $.get("templates/vehicle-tile.html", function(data){
-            tile = $.parseHTML(data);
-            $(tile).find('.brand').text(item.marca);
-            $(tile).find('.model').text(item.veiculo);
-            $(tile).find('.release-year').text(item.ano);
-            $('.list-options').append(tile);
-        });
-        
+        let tile = $.parseHTML(vehicleTemplate);
+        $(tile).attr('id', item.id);
+        $(tile).find('.brand').text(item.marca);
+        $(tile).find('.model').text(item.veiculo);
+        $(tile).find('.release-year').text(item.ano);
+        $('.list-options').append(tile);   
     }
+
+    function selectVehicleOnClick() {
+        $(".open-details").click(function() {
+            let clickedTile = $(this).parent();
+            let id = clickedTile.prop('id');
+            selectedVehicle = vehiclesToBeShown.filter(function(item){
+                return id == item.id;
+            })[0];
+            mountVehicleDetails();
+        })
+    }
+
+    function mountVehicleDetails() {
+        let v = selectedVehicle;
+        $('.details').find('h3').text(v.veiculo);
+        $('.details').find('.brand-value').text(v.marca);
+        $('.details').find('.year-value').text(v.ano);
+        $('.details').find('p').text(v.descricao);
+    }
+
 
     function addFunctionToButtons() {
         addSearchButtonFunction();
-        addNewVehicleButtonFunction();
-        addOpenVehicleDetailsFunction();
-        addEditVehicleFunction();
+        setNewVehicleModal();
+        setVehicleModal();
         addDeleteVehicleFunction();
     }
 
     function addSearchButtonFunction() {
         $(".search-button").click(function() {
             //get input
+            console.log('search added')
             let str;
             filterResultsByString(str);
         });
     }
 
-    function addNewVehicleButtonFunction() {
+    function setNewVehicleModal() {
         $(".add-vehicle").click(function() {
-            openVehicleModal();
+            //limpar campos
         });
     }
 
-    function addEditVehicleFunction() {
-        // $(".edit-button").click(function() {
-        //     //get ID
-        //     openVehicleModal(id);
-        // });
-    }
-
-    function addDeleteVehicleFunction() {
-        
+    function setVehicleModal() {
+        // usar selected modal
     }
 
     function addModalReferences() {
         $(".edit-modal").load('templates/add-edit-vehicle-modal.html');
-        $(".edit-modal").load('templates/delete-vehicle-modal.html');
-    }
-
-    function openVehicleModal(id) {
-        //copy template of modal
-        if(id) {
-            getVehicleById();
-        }
-        //should open a new one, or 
-        //open modal
-    }
-
-    function getVehicleById() {
-
+        $(".delete-modal").load('templates/delete-vehicle-modal.html');
     }
 
     function addDeleteVehicleFunction() {
         $(".edit-button").click(function() {
             //TODO
         });
-    }
-
-    function addOpenVehicleDetailsFunction() {
-
     }
 
     function filterResultsByString(str) {
